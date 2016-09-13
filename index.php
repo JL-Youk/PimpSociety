8e34cc8df2f5527f0a3065d753bf2572ef6b1f9c
JL <jeanlouis02300@gmail.com>
2016-09-14 01:37:20 +0200

    mise a jours du contenu

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script src="js/jquery.timeago.min.js"></script>
+    <script src="js/prism.js"></script>
+    <script src="jade/lunr.min.js"></script>
+    <script src="jade/search.js"></script>
+    <script src="js/init.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
   </head>
   <body>
@@ -31,20 +36,32 @@
      </div>
    </div>
  </nav>
+ <!--Fin menu -->
+ <!-- Slide1 -->
+ <div class="container">
+  <div class="section">
+    <div class="row">
+      <h1 class="col s12 light center header">Pimp Society</h1>
+      <h3 class="col s12 light center header">Offrez-vous des sites et applications sur mesure !</h3>
+    </div>
+  </div>
+</div>
+ <!--Fin  Slide1 -->
+<!--idée -->
 
-<!-- 1er -->
-<div class="container">
-        <div class="section">
-
-          <div class="row">
-            <h3 class="col s12 light center header">Offrez-vous un site sur mesure !</h3>
-          </div>
-
-
-
-        </div>
-
-      </div>
+<!--Fin idée -->
+ <div class="parallax-container">
+   <div class="parallax"><img src="images/laptop-1209008_1280.jpg"></div>
+  </div>
+  <div class="section white">
+    <div class="row container">
+      <h2 class="header">blabla</h2>
+      <p class="grey-text text-darken-3 lighten-3">blablablabal</p>
+    </div>
+  </div>
+  <div class="parallax-container">
+    <div class="parallax"><img src="images/computer-1209641_1920.jpg"></div>
+  </div>
 
 
 
@@ -53,12 +70,12 @@
   <div class="container">
     <form class="col s12">
       <div class="row">
-        <div class="input-field col s6">
+        <div class="input-field col m6 s12">
           <i class="material-icons prefix">account_circle</i>
-          <input id="icon_prefix" type="text" class="validate">
-          <label for="icon_prefix">First Name</label>
+          <input required id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">Nom</label>
         </div>
-        <div class="input-field col s6">
+        <div class="input-field col m6 s12">
           <i class="material-icons prefix">phone</i>
           <input id="icon_telephone" type="tel" class="validate">
           <label for="icon_telephone">Telephone</label>
@@ -66,20 +83,23 @@
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
-          <label for="email" data-error="wrong" data-success="right">Email</label>
+          <i class="material-icons prefix">email</i>
+          <input required id="email" type="email" class="validate">
+          <label for="email" data-error="wrong" data-success="right">Mail</label>
         </div>
       </div>
       <div class="row">
-        <div class="input-field col s6">
-          <i class="material-icons prefix">mode_edit</i>
-          <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-          <label for="icon_prefix2">First Name</label>
-        </div>
+          <div class="input-field col s12">
+            <i class="material-icons prefix">mode_edit</i>
+           <textarea id="textarea1" class="materialize-textarea"></textarea>
+           <label for="textarea1">Message</label>
+         </div>
+       </div>
+
+       <button class="btn waves-effect waves-light z-depth-4" type="submit" name="action">Submit
+         <i class="material-icons right">send</i>
+       </button>
       </div>
-      <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-       <i class="material-icons right">send</i>
-     </button>
     </form>
   </div>
 </div>
